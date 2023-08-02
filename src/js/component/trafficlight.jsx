b863f05927a123de6d388395a52e8c66277f08e9
María José Bogantes Murillo <88169247+mboga09@users.noreply.github.com>
2023-08-02 01:53:32 +0000

    Second commit

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -8,31 +8,50 @@ const TrafficLight = () => {
     const [greenOn, setGreenOn] = useState(false);
 
     return <>
-        <div className= "container">
-            <div className="palo">.</div>
-            <div className= "row d-flex justify-content-center">
-                <div className= "col d-flex justify-content-center flex-column align-items-center box">
-                    <div className= {redOn == false? "red" : "red on"} onClick={(e) => { 
-                        setRedOn(!redOn) 
-                        setGreenOn(false)
-                        setYellowOn(false)
-                    }}></div>
-                    <div className= {!yellowOn ? "yellow" : "yellow on"} onClick={(e) => { 
-                        setYellowOn(!yellowOn)
-                        setRedOn(false) 
-                        setGreenOn(false)
-                    }}></div>
-                    <div className= {!greenOn ? "green" : "green on"} onClick={(e) => { 
-                        setGreenOn(!greenOn)
-                        setRedOn(false) 
-                        setYellowOn(false)
-                    }}></div> {/*condicional ternario*/}
+        <body>
+            <div className= "container">
+
+                <div>
+
+                    <div className="palo">.</div>
+                    <div className= "d-flex justify-content-center flex-column align-items-center box shadow">
+                        <div className= {redOn == false? "red" : "red on"} onClick={(e) => { 
+                            setRedOn(!redOn) 
+                            setGreenOn(false)
+                            setYellowOn(false)
+                        }}></div>
+                        <div className= {!yellowOn ? "yellow" : "yellow on"} onClick={(e) => { 
+                            setYellowOn(!yellowOn)
+                            setRedOn(false) 
+                            setGreenOn(false)
+                        }}></div>
+                        <div className= {!greenOn ? "green" : "green on"} onClick={(e) => { 
+                            setGreenOn(!greenOn)
+                            setRedOn(false) 
+                            setYellowOn(false)
+                        }}></div> {/*condicional ternario*/}
+                    </div>
+
                 </div>
-            </div >
-            {redOn ? <h1>STOP!</h1> : <></>}
-            {yellowOn ? <h1>WAIT!</h1> : <></>}
-            {greenOn ? <h1>GO!</h1> : <></>}
-        </div>
+
+                <div className="d-flex justify-content-center pt-3">
+                    {redOn ? <h1>STOP!</h1> : <></>}
+                    {yellowOn ? <h1>WAIT!</h1> : <></>}
+                    {greenOn ? <h1>GO!</h1> : <></>}                
+                </div>
+
+                <div class="paper mt-4">
+                    <div class="paper-content">
+                        <div className="texto ms-3">Click the button to make the light cycle</div>
+                        <button type="button" class="btn btn-primary my-2 buttonStyle">Cycle</button>
+                        <div className="texto ms-3">Add an extra color!</div>
+                        <button type="button" class="btn btn-primary my-2 buttonStyle">Color</button>
+                    </div>
+                </div>
+
+            </div>
+        </body>
+
     </>
 }
 
